12a8be7c040f5bcc3e52ba4dfccd827cf671bb11
colttrain <colt.thurman@gmail.com>
2020-10-22 12:59:03 -0700

    Create Fibonacci.py

---
+++ Easy/Fibonacci.py
@@ -0,0 +1,11 @@
+def getNthFib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return getNthFib(n-1) + getNthFib(n-2)
+
+    pass
+
+print(getNthFib(6))
